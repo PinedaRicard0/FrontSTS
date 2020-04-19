e5ee46059d0925498582eb24f2c5efabe9a7fe38
Ricardo Pineda Rubio <pinedaricardo07@gmail.com>
2020-04-18 19:16:53 -0500

    Adding new drop for pools managment

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Category } from 'src/app/models/category.model';
 import { CategoriesService } from 'src/app/services/categories.service';
 import { Router } from '@angular/router';
-import { faUsersCog, faFlag } from '@fortawesome/free-solid-svg-icons';
+import { faUsersCog, faFlag, faListOl } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
@@ -14,9 +14,11 @@ export class MenuComponent implements OnInit {
   //Icons
   faTeams = faUsersCog;
   faField = faFlag;
+  faPool = faListOl;
   //End icons
   categories: Category[] = [];
   @ViewChild('teams') teamB: ElementRef;
+  @ViewChild('pools') poolB: ElementRef;
   constructor(private categoryService: CategoriesService, private router: Router) { }
 
   ngOnInit() {
@@ -28,7 +30,6 @@ export class MenuComponent implements OnInit {
   }
   
   onMenuButtonClick(option: string){
-    this.hideAllcollapsables();
     if(option == 'teams')
     {
       if(this.teamB.nativeElement.classList.contains('show')){
@@ -38,6 +39,14 @@ export class MenuComponent implements OnInit {
         this.teamB.nativeElement.classList.add('show')
       }
     }
+    else if(option == 'pools'){
+      if(this.poolB.nativeElement.classList.contains('show')){
+        this.poolB.nativeElement.classList.remove('show')
+      }
+      else{
+        this.poolB.nativeElement.classList.add('show')
+      }
+    }
     else if(option == 'fields'){
       this.router.navigate(['/fields']);
     }
@@ -45,9 +54,5 @@ export class MenuComponent implements OnInit {
 
   loadTeamsCategory(category: number){
     this.router.navigate(['/teams/' + category]);
-  }
-
-  hideAllcollapsables(){
-    this.teamB.nativeElement.classList.remove('show');
   }
 }
